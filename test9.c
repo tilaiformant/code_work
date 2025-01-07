298d55a2d0915b8c81adda85532aacd4ede22d1c
wujianyu <wujianyu322@gmail.com>
2025-01-07 22:57:32 +0800

    2-1的習題

--- test9.c
+++ test9.c
@@ -2,9 +2,13 @@
 
 int main()
 {
-    printf("*\n");
-    printf("***\n");
-    printf("*****\n");
-    printf("*******\n");
+    int i1, i2 ,i3;
+
+    i1=1;
+    i2=2;
+    i3=100;
+    printf("i1 = %d\n", i1);
+    printf("i2 = %d\n", i2);
+    printf("i3 = %d\n", i3);
     getchar();// 等待使用者輸入一個字元（按任意鍵）
 }
