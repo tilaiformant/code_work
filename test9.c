894eaf6ff8447758f0f8fbd3ea97ff42ff236c10
wujianyu <wujianyu322@gmail.com>
2025-01-09 15:04:59 +0800

    8進位與16進位應用

--- test9.c
+++ test9.c
@@ -1,16 +1,19 @@
-/*    ch2_3.c    */
+/*    ch2_4.c    */
 #include <stdio.h>
 
 int main()
 {   
-    printf("型態 = 大小 (bytes) \n");
-    printf("short = %d\n",sizeof(short));
-    printf("int = %d\n", sizeof(int));
-    printf("long = %d\n", sizeof(long));
-    printf("float = %d\n", sizeof(float));
-    printf("double = %d\n", sizeof(double));
-    printf("char =%d\n", sizeof(char));
-    
+    int i, j, k;
+    i = 013;
+    j = 026;
+    printf("i = %d\n", i);
+    printf("j = %d\n", j);
+    i = 0x28;
+    j = 0x3A;
+    k = 0x3a;
+    printf("i = %d\n", i);
+    printf("j = %d\n", j);
+    printf("k = %d\n", k);
     
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
