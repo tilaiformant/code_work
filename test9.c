163bf704160f57d601bb2b8000ede90da5618e99
wujianyu <wujianyu322@gmail.com>
2025-01-09 15:52:38 +0800

    設定a, b, c,的值是5

--- test9.c
+++ test9.c
@@ -1,15 +1,13 @@
-/*    ch2_5.c    */
+/*    ch2_5_1.c    */
 #include <stdio.h>
 
 int main()
 {   
-   int i;
-   char c;
-   c = 'a';
-   i = c + 1;
-   printf("c = %c\n", c);
+   int a, b, c;
+   a = b = c = 5;
+   printf("a = %d\n", a);
+   printf("b = %d\n", b);
    printf("c = %d\n", c);
-   printf("i = %d\n", i);
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
