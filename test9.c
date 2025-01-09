9762cd35cfff81ab209b165ac93b5b44dcbb487c
wujianyu <wujianyu322@gmail.com>
2025-01-09 17:27:40 +0800

    加,減,乘,除與求餘數

--- test9.c
+++ test9.c
@@ -1,13 +1,30 @@
-/*    ch2_5_1.c    */
+/*    ch2_6.c    */
 #include <stdio.h>
 
 int main()
 {   
-   int a, b, c;
-   a = b = c = 5;
-   printf("a = %d\n", a);
-   printf("b = %d\n", b);
-   printf("c = %d\n", c);
+   int s, a, b;
+
+   a = 10;
+   b = 15;
+   s = a + b;
+   printf("s = a + b = %d\n", s);
+   a = 1.8;
+   b = 2.3;
+   s = a - b;
+   printf("s = a - b = %d\n", s);
+   a = 5;
+   b = 6;
+   s = a * b;
+   printf("s = a * b = %d\n", s);
+   a = 2.4;
+   b = 1.2;
+   s = a / b;
+   printf("s = a / b = %d\n", s);
+   a = 5;
+   b = 4;
+   s = a % b;
+   printf("s = a %% b = %d\n", s);
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
