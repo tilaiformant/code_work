d85316b2424242e92d93213a584d8e0e319fe5f1
wujianyu <wujianyu322@gmail.com>
2025-01-10 13:44:09 +0800

    資料型態轉換3.0

--- test9.c
+++ test9.c
@@ -1,18 +1,17 @@
-/*    ch2_9.c    */
+/*    ch2_11.c    */
 #include <stdio.h>
 
 int main()
 {   
-   int a, b;
-   float s;
-   a = 3;
-   b = 2;
-   s = a / b;
-   printf("s = a / b = %3.2f\n", s);
-   a = 3;
-   b = 2;
-   s = (float) a / (float) b;
-   printf("s = (float) a / (float) b = %3.2f\n", s);
+   float a, b, s1;
+   int s2;
+
+   a = 4.6;
+   b = 2.1;
+   s1 = a / b;
+   printf("s1 = a / b = %3.2f\n", s1);
+   s2 = (int) a / (int) b;
+   printf("s2 = (int) a / (int) b = %d\n", s2);
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
