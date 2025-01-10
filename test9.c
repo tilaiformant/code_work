f5b73d7fe109ad687f2dceac3e03fc9501b2664b
wujianyu <wujianyu322@gmail.com>
2025-01-10 13:58:01 +0800

    整數和字元混合使用

--- test9.c
+++ test9.c
@@ -1,17 +1,12 @@
-/*    ch2_11.c    */
+/*    ch2_12.c    */
 #include <stdio.h>
 
 int main()
 {   
-   float a, b, s1;
-   int s2;
+   int i;
 
-   a = 4.6;
-   b = 2.1;
-   s1 = a / b;
-   printf("s1 = a / b = %3.2f\n", s1);
-   s2 = (int) a / (int) b;
-   printf("s2 = (int) a / (int) b = %d\n", s2);
+   i = 'a' - 'A';
+   printf("i = 'a' - 'A' = %d\n", i);
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
