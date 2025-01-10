828379e7c21d0b1eb2c099c5e673df4f046f3363
wujianyu <wujianyu322@gmail.com>
2025-01-10 12:24:39 +0800

    數學運算優先順序應用

--- test9.c
+++ test9.c
@@ -1,30 +1,15 @@
-/*    ch2_6.c    */
+/*    ch2_7.c    */
 #include <stdio.h>
 
 int main()
 {   
-   int s, a, b;
+   int s, a, b, c;
 
-   a = 10;
-   b = 15;
-   s = a + b;
-   printf("s = a + b = %d\n", s);
-   a = 1.8;
-   b = 2.3;
-   s = a - b;
-   printf("s = a - b = %d\n", s);
-   a = 5;
-   b = 6;
-   s = a * b;
-   printf("s = a * b = %d\n", s);
-   a = 2.4;
-   b = 1.2;
-   s = a / b;
-   printf("s = a / b = %d\n", s);
    a = 5;
    b = 4;
-   s = a % b;
-   printf("s = a %% b = %d\n", s);
+   c = 3;
+   s = a * b % c;
+   printf("s = a * b %% c = %d\n", s);
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
