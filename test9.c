7dd6bfd88606b53bb061873509e19ccf8345fcb0
wujianyu <wujianyu322@gmail.com>
2025-01-10 17:22:35 +0800

    計算圓周率收斂

--- test9.c
+++ test9.c
@@ -1,13 +1,12 @@
-/*    ch2_15.c    */
+/*    ch2_16.c    */
 #include <stdio.h>
 
 int main()
 {   
-   float r = 2.0;
-   float pi = 3.1415926;
-   float area;
-   area = pi * r * r;
-   printf("圓面積是 %f \n", area);
+    double pi;
+    pi = 4 * (1 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9);
+    printf("pi = %f \n", pi);
+
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
 }
