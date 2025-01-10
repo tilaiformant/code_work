d12beac5832101244ed302700d672ee65da56859
wujianyu <wujianyu322@gmail.com>
2025-01-10 14:09:20 +0800

    開學買球鞋程式

--- test9.c
+++ test9.c
@@ -1,12 +1,14 @@
-/*    ch2_12.c    */
+/*    ch2_12_1.c    */
 #include <stdio.h>
 
 int main()
 {   
-   int i;
+    int size;
+    float foot = 7.5;   /* 腳的尺寸 */
 
-   i = 'a' - 'A';
-   printf("i = 'a' - 'A' = %d\n", i);
+    size = (int) foot + 1;
+    printf("你的腳的尺寸是     : %2.1f\n", foot);
+    printf("你購買的鞋子尺寸是 : %d\n", size); 
 
     getchar( );// 等待使用者輸入一個字元（按任意鍵）
     return 0;
